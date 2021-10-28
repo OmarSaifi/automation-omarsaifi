c532cd509fb83b8662b217402312c0dacb718759
Omar Saifi <omar.saifi@yh.nackademin.se>
2021-10-28 23:48:22 +0200

    Created a first test case

--- frontend-tests/cypress/integration/test-suite.spec.js
+++ frontend-tests/cypress/integration/test-suite.spec.js
@@ -3,12 +3,18 @@
 // Test suite
 describe('Test suite', function(){
 
-    // Test case
+    // Test case 1
     it('test case 1', function(){
         cy.visit('http://localhost:3000')
-
-
+        cy.contains('Login')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.contains('Rooms')
+        cy.get('.user > .btn').click()
     })
+    
 
 
 })
