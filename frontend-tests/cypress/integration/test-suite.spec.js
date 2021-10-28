8c597429b4251d5f183190bc367368c1576d8381
Omar Saifi <omar.saifi@yh.nackademin.se>
2021-10-28 22:51:09 +0200

    Created a testproject, 2 more bash and .gitignore

---
+++ frontend-tests/cypress/integration/test-suite.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+// Test suite
+describe('Test suite', function(){
+
+    // Test case
+    it('test case 1', function(){
+        cy.visit('http://localhost:3000')
+
+
+    })
+
+
+})
