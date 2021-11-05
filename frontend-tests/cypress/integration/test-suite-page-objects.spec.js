0efc072457167106fdfae0071c195272c3d20a10
Omar Saifi <omar.saifi@yh.nackademin.se>
2021-11-05 16:41:01 +0100

    beforeEach and afterEach works and the 1 test case does not work beacause its Login and Logout

--- frontend-tests/cypress/integration/test-suite-page-objects.spec.js
+++ frontend-tests/cypress/integration/test-suite-page-objects.spec.js
@@ -12,51 +12,51 @@ import * as reservationFuncs from '../pages/createReservationPage'
 // Test suite
 describe('Test Suite', function(){
 
-    before(()=>{
+    beforeEach(()=>{
         cy.visit(targets.base_url)
         indexFuncs.checkTitleOfIndexPage(cy)
-        //indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
+        indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
     })
 
     afterEach(()=>{
-        //dashboardFuncs.performLogout(cy, 'Login')
-    })
-    
-    // Test case 1
-    it('Test case 1 - Perform login and logout', function(){
-        indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
         dashboardFuncs.performLogout(cy, 'Login')
     })
+    
+    // Test case 1 - With my last commit - This Case 1 does not work because beforeEach and afterEach is included and works.
+    //it('Test case 1 - Perform login and logout', function(){
+        //indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
+        //dashboardFuncs.performLogout(cy, 'Login')
+    //})
 
     // Test case 2
     it('Test case 2 - Create a new room', function(){
-        indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
+        //indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
         roomFuncs.viewRoom(cy)
         roomFuncs.createNewRoom(cy)
-        dashboardFuncs.performLogout(cy, 'Login')
+        //dashboardFuncs.performLogout(cy, 'Login')
     })
 
     // Test case 3
     it('Test case 3 - Create a new Client', function(){
-        indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
+        //indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
         clientFuncs.viewClient(cy)
         clientFuncs.createNewClient(cy)
-        dashboardFuncs.performLogout(cy, 'Login')
+        //dashboardFuncs.performLogout(cy, 'Login')
     })
 
     // Test case 4
     it('Test case 4 - Create a new Bill', function(){
-        indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
+        //indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
         billFuncs.viewBill(cy)
         billFuncs.createNewBill(cy)
-        dashboardFuncs.performLogout(cy, 'Login')
+        //dashboardFuncs.performLogout(cy, 'Login')
     })
 
     // Test case 5
     it('Test case 5 - Create a new Reservation', function(){
-        indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
+        //indexFuncs.performValidLogin(cy, targets.username, targets.password, 'Tester Hotel Overview')
         reservationFuncs.viewReservation(cy)
         reservationFuncs.createNewReservation(cy)
-        dashboardFuncs.performLogout(cy, 'Login')
+        //dashboardFuncs.performLogout(cy, 'Login')
     })
 })
